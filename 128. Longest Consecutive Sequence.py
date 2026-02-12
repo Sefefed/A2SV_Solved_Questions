9e560d5e7f534665bc910c54584f6d7ff9ea3ce0
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-02-12 08:45:09 +0300

    Add solution for longest consecutive sequence problem
    Implement a solution to find the longest consecutive sequence in an array of integers.

---
+++ 128. Longest Consecutive Sequence.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_set = set(nums)
+        max_len = 0
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                cur_len = 1
+                cur_num = num
+                while cur_num + 1 in nums_set:
+                    cur_len += 1
+                    cur_num += 1
+                max_len = max(max_len, cur_len)   
+        return max_len
+ 
+        
