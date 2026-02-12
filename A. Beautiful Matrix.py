426539e769a72c087cc51c2800e00a56434f891d
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-02-12 17:20:27 +0300

    Add logic to process multiple input lists for '1'
    Implement logic to find the position of '1' in multiple lists and print the result based on its index.

---
+++ A. Beautiful Matrix.py
@@ -0,0 +1,20 @@
+list1 = list(map(int, input().split()))
+if 1 in list1:
+  print(2 + abs(2 - list1.index(1)))
+  exit()
+list2 = list(map(int, input().split()))
+if 1 in list2:
+  print(1 + abs(2 - list2.index(1)))
+  exit()
+list3 = list(map(int, input().split()))
+if 1 in list3:
+  print(0 + abs(2 - list3.index(1)))
+  exit()
+list4 = list(map(int, input().split()))
+if 1 in list4:
+  print(1 + abs(2 - list4.index(1)))
+  exit()
+list5 = list(map(int, input().split()))
+if 1 in list5:
+  print(2 + abs(2 - list5.index(1)))
+  exit()
