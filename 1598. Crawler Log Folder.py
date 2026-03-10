e3e8770c7601f47c9665fa1e78fda33aeac071e2
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-03-10 16:32:07 +0300

    Implement minOperations to process crawler logs

---
+++ 1598. Crawler Log Folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        st = []
+        for log in logs:
+            if log == "../":
+                if st:
+                    st.pop()
+                else:
+                    pass    
+            elif log == "./":
+                pass
+            else:
+                st.append(log)
+        return len(st)               
