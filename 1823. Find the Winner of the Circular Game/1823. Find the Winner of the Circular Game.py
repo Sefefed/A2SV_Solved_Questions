2214e10881c45e2d99f762bda51e364f5a8ef685
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-03-26 15:23:53 +0300

    Implement circular game winner finding algorithm

---
+++ 1823. Find the Winner of the Circular Game/1823. Find the Winner of the Circular Game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        players = [i+1 for i in range(n)]
+        def winner(ind, players):
+            if len(players) == 1:
+                return players[0]
+            indices =  (ind+k-1) % len(players)   
+            players.pop(indices)
+            return winner(indices, players) 
+        return winner(0, players)    
