065ddec6833999cad911918c7af30227b9e7e0df
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-04-27 01:07:10 +0300

    Add solution for checking bipartite graph
    Implement DFS to check if a graph is bipartite.

---
+++ 785. Is Graph Bipartite?.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [0] * n
+        def dfs(node, c):
+            color[node] = c
+            for nei in graph[node]:
+                if color[nei] == 0:
+                    if not dfs(nei, -c):
+                        return False
+                elif color[nei] == color[node]:
+                    return False
+            return True
+        for i in range(n):
+            if color[i] == 0:
+                if not dfs(i, 1):
+                    return False
+        return True                               
+        
