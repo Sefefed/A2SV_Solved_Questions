39b78dada832d8e6ed3a106b82b740f161f98ce6
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-05-22 16:25:46 +0300

    Create 337. House Robber III.py

---
+++ 337. House Robber III.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        memo = {}
+        def dp(root):
+            if not root:
+                return 0
+            if root not in memo:
+                ntake = dp(root.left) + dp(root.right)
+                take = root.val
+                if root.left:
+                    take += dp(root.left.left) + dp(root.left.right)
+                if root.right:
+                    take += dp(root.right.left) + dp(root.right.right)    
+                memo[root] = max(ntake, take)
+            return memo[root]    
+        return dp(root)    
+
+
+
+        
