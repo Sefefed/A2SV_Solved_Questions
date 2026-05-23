f0ddd95dbb81d7023cfedd15aebe461d83785560
SEFEFE DAGNE <sefefedagne6@gmail.com>
2026-05-23 23:16:08 +0300

    Create A - Frog 1.py

---
+++ A - Frog 1.py
@@ -0,0 +1,8 @@
+n = int(input())
+hgt = list(map(int, input().split()))
+dp = [0] * n
+dp[1] = abs(hgt[1] - hgt[0])
+for i in range(2, n):
+  dp[i] = min(dp[i - 1] + abs(hgt[i] - hgt[i - 1]), dp[i - 2] + abs(hgt[i] - hgt[i - 2]))
+print(dp[n - 1])  
+  
